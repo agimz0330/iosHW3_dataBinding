e1605d9586ce11080db7f64736794aae6b5814c7
c60603 <c60603@gmail.com>
2020-10-28 14:32:16 +0800

    Initial Commit

---
+++ iosHW3/iosHW3App.swift
@@ -0,0 +1,17 @@
+//
+//  iosHW3App.swift
+//  iosHW3
+//
+//  Created by User08 on 2020/10/28.
+//
+
+import SwiftUI
+
+@main
+struct iosHW3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
